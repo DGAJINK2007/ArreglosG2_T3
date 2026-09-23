11abdcd989c6420f6da80836f649f6ad8cfc7b5c
DGAJINK2007 <25030109@itesa.edu.mx>
2026-09-23 15:00:11 -0600

    Version_03:  Metodo ordenar ascendente, descendente.

--- Arreglos.UI.Consola/Program.cs
+++ Arreglos.UI.Consola/Program.cs
@@ -1,8 +1,27 @@
-﻿internal class Program
+﻿using Arreglos.Logica;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("\nArreglos");
+
+        MiArreglo oMiArreglo = new MiArreglo(10);
+
+        oMiArreglo.Llenar(5, 20);
+
+        Console.WriteLine("\nDatos desordenados");
+        Console.WriteLine(oMiArreglo);
+
+        Console.WriteLine("\nDatos ordenados ascendete");
+        oMiArreglo.Ordenar();
+        Console.WriteLine(oMiArreglo);
+
+        Console.WriteLine("\nDatos ordenados descendete");
+        oMiArreglo.Ordenar(false);
+        Console.WriteLine(oMiArreglo);
+
+
         Console.ReadKey();
     }
 }
